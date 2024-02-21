467008994affc8344c4654955bdea6247f2ae2f2
pryan <pat.ryan@health.govt.nz>
2024-02-21 23:00:50 +0000

    added constants

--- openapi/makeoas.sh
+++ openapi/makeoas.sh
@@ -18,19 +18,23 @@ getPomProperty() {
 #!/bin/bash
 set -x #echo off
  
- common_version=$(getPomProperty "fhir-common.version")
- ig_version=$(getPomProperty "revision")
- echo "!!!!!!!!!!!!! ig_version = $ig_version"
+# constants
+IG_FILENAME=HipFhirNesOpenApi.yaml
+PATHS_FILENAME=HipFhirNesPaths.yaml
+
+common_version=$(getPomProperty "fhir-common.version")
+ig_version=$(getPomProperty "revision")
+echo "!!!!!!!!!!!!! ig_version = $ig_version"
  
-cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/HipFhirNesOpenApi.yaml
+cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/$IG_FILENAME
 
 #add the MWS paths to the common openapi spec
-newpath=$(< ./openapi/HipFhirNesPaths.yaml)  yq -i '.paths=env(newpath) ' openapi/HipFhirNesOpenApi.yaml 
-newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newversion) ' openapi/HipFhirNesOpenApi.yaml 
+newpath=$(< ./openapi/$PATHS_FILENAME)  yq -i '.paths=env(newpath) ' openapi/$IG_FILENAME 
+newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newversion) ' openapi/$IG_FILENAME
 
 yq -i 'del(.paths[][].x-amazon-apigateway-integration)' openapi/HipFhirNesOpenApi.yaml
 yq -i '.info.title="HIP NES FHIR"'  openapi/HipFhirNesOpenApi.yaml
-yq -i '.info.description="Te Whatu Ora Enrolment and Entitlement FHIR services"'  openapi/HipFhirNesOpenApi.yaml
-version=$ig_version  yq -i '.info.version = env(version)'  openapi/HipFhirNesOpenApi.yaml
-zip -u ./input/extra/HipFhirNesOpenApi.zip ./openapi/HipFhirNesOpenApi.yaml 
+yq -i '.info.description="Te Whatu Ora Enrolment and Entitlement FHIR services"'  openapi/$IG_FILENAME
+version=$ig_version  yq -i '.info.version = env(version)'  openapi/$IG_FILENAME
+zip -u ./input/extra/HipFhirNesOpenApi.zip ./openapi/$IG_FILENAME
 
