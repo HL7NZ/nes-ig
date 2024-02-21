8e921af848a396d945cdd0ddb3e2121394d78a9a
pryan <pat.ryan@health.govt.nz>
2024-02-21 22:54:29 +0000

    getting ig version from pom

--- openapi/makeoas.sh
+++ openapi/makeoas.sh
@@ -1,14 +1,26 @@
+
+getPomProperty() {
+ 
+ #echo "getting value of $1 from pom"
+ line=$(grep $1.*$1 pom.xml  | grep -v '!' | tr -d '\t ')
+
+ plength=${#1}
+ offset=2
+ plength=$(($plength+$offset))
+ sline=${line:plength:200}
+ #trim trailing chars
+ property=${sline%%</$1>}
+ #echo "property $1 = $property"
+ echo $property
+ }
+
+
 #!/bin/bash
 set -x #echo off
-
-
-# get the version from the pom
- vline=$(grep 'fhir-common.version.*fhir-common.version' pom.xml | grep -v '!' )
- #trim prefix
- common_version=${vline:23:200}
- #trim trailign chars
- common_version=${common_version%%</fhir-common.version>}
- echo "fhir-common.version = $common_version"
+ 
+ common_version=$(getPomProperty "fhir-common.version")
+ ig_version=$(getPomProperty "revision")
+ echo "!!!!!!!!!!!!! ig_version = $ig_version"
  
 cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/HipFhirNesOpenApi.yaml
 
@@ -19,6 +31,6 @@ newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newve
 yq -i 'del(.paths[][].x-amazon-apigateway-integration)' openapi/HipFhirNesOpenApi.yaml
 yq -i '.info.title="HIP NES FHIR"'  openapi/HipFhirNesOpenApi.yaml
 yq -i '.info.description="Te Whatu Ora Enrolment and Entitlement FHIR services"'  openapi/HipFhirNesOpenApi.yaml
-
+version=$ig_version  yq -i '.info.version = env(version)'  openapi/HipFhirNesOpenApi.yaml
 zip -u ./input/extra/HipFhirNesOpenApi.zip ./openapi/HipFhirNesOpenApi.yaml 
 
