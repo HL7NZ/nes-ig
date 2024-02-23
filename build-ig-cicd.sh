927232806ce0cba9569caa771cd2fe91e0a66b26
pryan <pat.ryan@health.govt.nz>
2024-02-23 04:55:35 +0000

    fixed package

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -72,7 +72,7 @@ echo getting NHI dependencies...
 nhi_package_name="hl7.org.nz.fhir.ig.nhi"
 nhi_version=$(yq '.dependencies."hl7.org.nz.fhir.ig.nhi".version' ./sushi-config.yaml)
 nhi_pom_version=$(getPomProperty "nhi-ig-version")
-nhi_source=./fhir_packages/hip-nhi-conformance-module-$nhi_pom_version/package.tgz
+nhi_source=./fhir_packages/hip-nhi-conformance-module-$nhi_pom_version/package/package.tgz
 nhi_url=$(yq '.dependencies."hl7.org.nz.fhir.ig.nhi".uri' ./sushi-config.yaml)
 addPackage "$nhi_package_name" "$nhi_version" "$nhi_source" "$nhi_url"
 
@@ -82,7 +82,7 @@ hpi_package_name="hl7.org.nz.fhir.ig.hpi"
 hpi_url=$(yq '.dependencies."hl7.org.nz.fhir.ig.hpi".uri' ./sushi-config.yaml)
 hpi_version=$(yq '.dependencies."hl7.org.nz.fhir.ig.hpi".version' ./sushi-config.yaml)
 hpi_pom_version=$(getPomProperty "hpi-ig-version")
-hpi_source=./fhir_packages/hip-hpi-conformance-module-$hpi_pom_version/package.tgz
+hpi_source=./fhir_packages/hip-hpi-conformance-module-$hpi_pom_version/package/package.tgz
 hpi_url=$(yq '.dependencies."hl7.org.nz.fhir.ig.hpi".uri' ./sushi-config.yaml)
 addPackage "$hpi_package_name" "$hpi_version" "$hpi_source" "$hpi_url"
 
