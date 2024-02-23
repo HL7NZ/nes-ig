7971d0fba9314874f96904c373a3a9fbb2a7097d
pryan <pat.ryan@health.govt.nz>
2024-02-23 02:28:11 +0000

    remove aws validator

--- openapi/makeoas.sh
+++ openapi/makeoas.sh
@@ -19,22 +19,24 @@ getPomProperty() {
 set -x #echo off
  
 # constants
-IG_FILENAME=HipFhirNesOpenApi.yaml
+IG_FILENAME=HipFhirNesOpenApi
 PATHS_FILENAME=HipFhirNesPaths.yaml
 
 common_version=$(getPomProperty "fhir-common.version")
 ig_version=$(getPomProperty "revision")
-echo "!!!!!!!!!!!!! ig_version = $ig_version"
+echo "ig_version = $ig_version"
  
-cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/$IG_FILENAME
+cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/$IG_FILENAME.yaml
 
-#add the MWS paths to the common openapi spec
-newpath=$(< ./openapi/$PATHS_FILENAME)  yq -i '.paths=env(newpath) ' openapi/$IG_FILENAME 
-newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newversion) ' openapi/$IG_FILENAME
+#add the NES paths to the common openapi spec
+newpath=$(< ./openapi/$PATHS_FILENAME)  yq -i '.paths=env(newpath) ' openapi/$IG_FILENAME.yaml 
+newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newversion) ' openapi/$IG_FILENAME.yaml
 
-yq -i 'del(.paths[][].x-amazon-apigateway-integration)' openapi/HipFhirNesOpenApi.yaml
-yq -i '.info.title="HIP NES FHIR"'  openapi/HipFhirNesOpenApi.yaml
-yq -i '.info.description="Te Whatu Ora Enrolment and Entitlement FHIR services"'  openapi/$IG_FILENAME
-version=$ig_version  yq -i '.info.version = env(version)'  openapi/$IG_FILENAME
-zip -u ./input/extra/HipFhirNesOpenApi.zip ./openapi/$IG_FILENAME
+yq -i 'del(.paths[][].x-amazon-apigateway-integration)' openapi/$IG_FILENAME.yaml
+yq -i 'del(.paths[][].x-amazon-apigateway-request-validator)' openapi/$IG_FILENAME.yaml
+
+yq -i '.info.title="HIP NES FHIR"'  openapi/$IG_FILENAME.yaml
+yq -i '.info.description="Te Whatu Ora Enrolment and Entitlement FHIR services"'  openapi/$IG_FILENAME.yaml
+version=$ig_version  yq -i '.info.version = env(version)'  openapi/$IG_FILENAME.yaml
+zip -u ./input/extra/$IG_FILENAME.zip ./openapi/$IG_FILENAME.yaml
 
