6f049c90bc01590efb2090e24c39430be29956d9
pryan <pat.ryan@health.govt.nz>
2024-03-01 03:16:19 +0000

    remove dev

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -63,8 +63,8 @@ ls -l ./fhir_packages/
 common_name="hl7.org.nz.fhir.ig.hip-core"
 common_version=$(yq '.dependencies."hl7.org.nz.fhir.ig.hip-core".version' ./sushi-config.yaml)
 
-comdir=$(ls -d ./fhir_packages/hip-fhir-common*)
-common_source="$comdir/package/package.tgz"
+#comdir=$(ls -d ./fhir_packages/hip-fhir-common*)
+common_source="./fhir_packages/hip-fhir-common-$common_version/package/package.tgz"
 common_url=$(yq '.dependencies."hl7.org.nz.fhir.ig.hip-core".uri' ./sushi-config.yaml)
 addPackage "$common_name" "$common_version" "$common_source" "$common_url" 
 
