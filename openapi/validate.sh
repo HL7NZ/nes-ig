f08db2c59fe92ba7335d027ee7e12a56ae8a242f
pryan <pat.ryan@health.govt.nz>
2026-05-15 06:05:37 +0000

    added Flag Test

--- openapi/validate.sh
+++ openapi/validate.sh
@@ -10,6 +10,13 @@ do
 done
  openapi-examples-validator --no-additional-properties -s $.components.schemas.EpisodeOfCare -e  ./updateEnrolmentRequest.json ./HipFhirNesOpenApi.yaml
 	
+for FILE in  ../fsh-generated/resources/*Flag-*.json
+do
+	openapi-examples-validator --no-additional-properties -s $.components.schemas.Flag -e  ${FILE} ./HipFhirNesOpenApi.yaml
+done
+ 
+	
+
 
 #FHIR validator
 
